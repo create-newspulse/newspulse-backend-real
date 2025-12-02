de523c67712bc51cf7326bf9f42fc8d6437ce3c9
create-newspulse <newspulse.ads@gmail.com>
2025-12-02 19:10:10 +0530

    chore(cors): allow Expo dev origins (http://localhost:8081, 19006)\n\n- Extend Express CORS allowlist\n- Extend Socket.IO CORS origins\n- Preserve all existing production origins

--- server.js
+++ server.js
@@ -84,6 +84,8 @@ const defaultOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:3000',
+  'http://localhost:8081',
+  'http://localhost:19006',
   'https://admin.newspulse.co.in',
   'https://newspulse-frontend-main.vercel.app',
   'https://newspulse.co.in',
@@ -693,6 +695,8 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:3000',
+      'http://localhost:8081',
+      'http://localhost:19006',
       'https://newspulse-frontend-main.vercel.app',
       'https://admin.newspulse.co.in',
     ],
