6d9f9ad1307e3355ab478a1b2958da36d5ac458f
create-newspulse <newspulse.ads@gmail.com>
2025-12-02 19:19:33 +0530

    chore(cors): add 127.0.0.1 Expo dev origins (8081, 19006)\n\n- Complements localhost entries for stricter origin matching

--- server.js
+++ server.js
@@ -86,6 +86,8 @@ const defaultOrigins = [
   'http://localhost:3000',
   'http://localhost:8081',
   'http://localhost:19006',
+  'http://127.0.0.1:8081',
+  'http://127.0.0.1:19006',
   'https://admin.newspulse.co.in',
   'https://newspulse-frontend-main.vercel.app',
   'https://newspulse.co.in',
